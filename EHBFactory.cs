70f5da200c681c37d77447755bb29f373ab3dee8
plokmijnuhby <39633434+plokmijnuhby@users.noreply.github.com>
2020-02-27 19:53:38 +0000

    Add files via upload

--- EHBFactory.cs
+++ EHBFactory.cs
@@ -13,9 +13,9 @@ namespace EHBSplitter
         public string ComponentName => "else Heart.Break() Autosplitter";
         public string Description => "Automates splits for else Heart.Break()";
         public ComponentCategory Category => ComponentCategory.Control;
-        public string UpdateName => "else Heart.Break()";
+        public string UpdateName => ComponentName;
         public string XMLURL => UpdateURL + "EHB-Splitter.xml";
         public string UpdateURL => "https://raw.githubusercontent.com/plokmijnuhby/EHB-Autosplitter/master/";
-        public Version Version => new Version("1.0");
+        public Version Version => new Version("1.0.0");
     }
 }
